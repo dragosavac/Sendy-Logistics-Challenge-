725ca7f099c1ee124dbdd488c6cc55d3610c2c8e
Rade Dragosavac <rade.dragosavac@symphony.is>
2019-11-01 16:44:43 +0100

    Adding training model jypn file and fixing errors

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestRegressor
 
 
 riders_data = pd.read_csv('Data/Riders.csv')
@@ -7,9 +8,9 @@ train_data = pd.read_csv('Data/Train.csv')
 test_data = pd.read_csv('Data/Test.csv')
 
 
-def time_from_midnight_in_seconds(column):
-    train_data[column] = pd.to_datetime(train_data[column])
-    return (train_data[column] - pd.to_datetime(pd.to_datetime('today').date())).astype('timedelta64[s]')
+def time_from_midnight_in_seconds(dataset, column):
+    dataset[column] = pd.to_datetime(dataset[column])
+    return (dataset[column] - pd.to_datetime(pd.to_datetime('today').date())).astype('timedelta64[s]')
 
 
 def delta_time(dataset, higher_time, lower_time):
@@ -21,19 +22,17 @@ def average(list_data):
 
 
 # converting time in seconds from midnight for train data
-train_data['Placement - Time'] = time_from_midnight_in_seconds('Placement - Time')
-train_data['Confirmation - Time'] = time_from_midnight_in_seconds('Confirmation - Time')
-train_data['Pickup - Time'] = time_from_midnight_in_seconds('Pickup - Time')
-train_data['Arrival at Pickup - Time'] = time_from_midnight_in_seconds('Arrival at Pickup - Time')
-train_data['Arrival at Destination - Time'] = time_from_midnight_in_seconds('Arrival at Destination - Time')
+train_data['Placement - Time'] = time_from_midnight_in_seconds(train_data, 'Placement - Time')
+train_data['Confirmation - Time'] = time_from_midnight_in_seconds(train_data, 'Confirmation - Time')
+train_data['Pickup - Time'] = time_from_midnight_in_seconds(train_data, 'Pickup - Time')
+train_data['Arrival at Pickup - Time'] = time_from_midnight_in_seconds(train_data, 'Arrival at Pickup - Time')
+train_data['Arrival at Destination - Time'] = time_from_midnight_in_seconds(train_data, 'Arrival at Destination - Time')
 
 # converting time in seconds from midnight for test data
-test_data['Placement - Time'] = time_from_midnight_in_seconds('Placement - Time')
-test_data['Confirmation - Time'] = time_from_midnight_in_seconds('Confirmation - Time')
-test_data['Pickup - Time'] = time_from_midnight_in_seconds('Pickup - Time')
-test_data['Arrival at Pickup - Time'] = time_from_midnight_in_seconds('Arrival at Pickup - Time')
-test_data['Arrival at Destination - Time'] = time_from_midnight_in_seconds('Arrival at Destination - Time')
-
+test_data['Placement - Time'] = time_from_midnight_in_seconds(test_data, 'Placement - Time')
+test_data['Confirmation - Time'] = time_from_midnight_in_seconds(test_data, 'Confirmation - Time')
+test_data['Pickup - Time'] = time_from_midnight_in_seconds(test_data, 'Pickup - Time')
+test_data['Arrival at Pickup - Time'] = time_from_midnight_in_seconds(test_data, 'Arrival at Pickup - Time')
 
 # calculating delta_time for train data
 delta_confirm_place_train = delta_time(train_data, 'Confirmation - Time', 'Placement - Time')
@@ -46,15 +45,13 @@ delta_placement_arrival_train = delta_time(train_data, 'Arrival at Destination -
 delta_confirm_place_test = delta_time(test_data, 'Confirmation - Time', 'Placement - Time')
 delta_pick_arr_confirm_test = delta_time(test_data, 'Arrival at Pickup - Time', 'Confirmation - Time')
 delta_pickup_confirm_test = delta_time(test_data, 'Pickup - Time', 'Arrival at Pickup - Time')
-delta_arrival_pickup_test = delta_time(test_data, 'Arrival at Destination - Time', 'Pickup - Time')
-delta_placement_arrival_test = delta_time(test_data, 'Arrival at Destination - Time', 'Placement - Time')
 
 # merging riders data with train data and test data
 train_with_rider_info = train_data.merge(riders_data, on='Rider Id')
 test_with_rider_info = test_data.merge(riders_data, on='Rider Id')
 
 # adding Time from Pickup to Arrival column in test data
-test_with_rider_info['Time from Pickup to Arrival'] = delta_arrival_pickup_test
+# test_with_rider_info['Time from Pickup to Arrival'] = delta_arrival_pickup_test
 
 # drop outliers
 train_with_rider_info = train_with_rider_info[train_with_rider_info['Placement - Weekday (Mo = 1)'] == train_with_rider_info['Confirmation - Weekday (Mo = 1)']]
@@ -73,7 +70,7 @@ labelencoder_personal_business = LabelEncoder()
 test_with_rider_info['Personal or Business'] = labelencoder_personal_business.fit_transform(test_with_rider_info['Personal or Business'])
 
 
-# one hot encoding of the train_data['Personal or Business'] column
+# one hot encoding of the train_data['Platform Type'] column
 train_with_rider_info['Platform Type'] = train_with_rider_info['Platform Type'].astype('category')
 train_with_rider_info = pd.concat([train_with_rider_info.drop(columns=['Platform Type']), pd.get_dummies(train_with_rider_info['Platform Type'])], axis=1)
 
@@ -88,13 +85,17 @@ train_with_rider_info.drop(columns=['Confirmation - Day of Month', 'Confirmation
                                     'Pickup - Day of Month', 'Pickup - Weekday (Mo = 1)',
                                     'Arrival at Destination - Day of Month',
                                     'Arrival at Destination - Weekday (Mo = 1)',
-                                    'Vehicle Type'], inplace=True)
+                                    'Vehicle Type', 'Order No',
+                                    'User Id', 'Rider Id',
+                                    'Precipitation in millimeters'], inplace=True)
 
 # drop redundant columns test data
 test_with_rider_info.drop(columns=['Confirmation - Day of Month', 'Confirmation - Weekday (Mo = 1)',
                                    'Arrival at Pickup - Day of Month', 'Arrival at Pickup - Weekday (Mo = 1)',
                                    'Pickup - Day of Month', 'Pickup - Weekday (Mo = 1)',
-                                   'Vehicle Type'], inplace=True)
+                                   'Vehicle Type', 'Order No',
+                                   'User Id', 'Rider Id',
+                                   'Precipitation in millimeters'], inplace=True)
 
 
 # renaming columns
@@ -111,5 +112,3 @@ train_with_rider_info.rename(columns={'Placement - Day of Month': 'Day of Month'
 test_with_rider_info.rename(columns={'Placement - Day of Month': 'Day of Month',
                                      'Placement - Weekday (Mo = 1)': 'Weekday (Mo = 1)'})
 
-print(train_with_rider_info.shape)
-print(test_with_rider_info.shape)
